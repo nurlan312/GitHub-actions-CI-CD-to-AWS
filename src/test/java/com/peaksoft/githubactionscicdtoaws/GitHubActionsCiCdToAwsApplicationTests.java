21791e4c54d56fd7de98432e98c749e2c43ecbb5
Nurlan <nurlanasj@gmail.com>
2022-07-05 14:05:23 +0300

    CI/CD

--- src/test/java/com/peaksoft/githubactionscicdtoaws/GitHubActionsCiCdToAwsApplicationTests.java
+++ src/test/java/com/peaksoft/githubactionscicdtoaws/GitHubActionsCiCdToAwsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.peaksoft.githubactionscicdtoaws;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GitHubActionsCiCdToAwsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.peaksoft.githubactionscicdtoaws;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GitHubActionsCiCdToAwsApplicationTests {
+//
+////    @Test
+//    void contextLoads() {
+//    }
+//
+//}
